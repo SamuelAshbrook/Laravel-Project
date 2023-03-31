6d4da36ea54e39fafa0166c68b8b9537c555a8a3
SamuelAshbrook <ashbrook444@gmail.com>
2023-03-31 14:59:21 +0100

    Laravel Fundamentals - Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Get the welcome view on home URL
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Get some text on /about slug
+Route::get('/about', function () {
+    return "About Page";
+});
+
+// Get some text on /contact slug
+Route::get('/contact', function () {
+    return "Contact Page";
+});
+
+// Get some text using variables and function parameters
+Route::get("/post/{id}/{name}", function($id, $name){
+    return "This is post number: " . $id . "" . $name;
+});
+
+// Assign name to URL
+Route::get("/admin/posts/example", array('as'=>'admin.home', function(){
+    $url = route('admin.home');
+    return "This url is " . $url;
+}));
